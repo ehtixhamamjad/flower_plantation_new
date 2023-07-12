080502a2089a2ff3bcbafa7210ed8ffde6ce1350
Ehtixham Amjad <33822563+ehtixhamamjad@users.noreply.github.com>
2023-07-12 16:35:10 +0500

    inital commit

---
+++ lib/screen/bar_screen/bar_view_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class BarViewScreen extends StatefulWidget {
+  const BarViewScreen({super.key});
+
+  @override
+  State<BarViewScreen> createState() => _BarViewScreenState();
+}
+
+class _BarViewScreenState extends State<BarViewScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
